247421724caa34ddc8169ab8daae6a6cb10c847a
haoran <haoranyang@sohu-inc.com>
2020-12-08 15:19:41 +0800

    rename

--- lib/router_gen.dart
+++ lib/router_gen.dart
@@ -1,4 +1,4 @@
 library router_gen;
-export 'src/annotation/router.dart';
+export 'src/annotation/router_page.dart';
 export 'src/annotation/router_table.dart';
 export 'src/annotation/router_arg.dart';
--- lib/src/generator/router_generator.dart
+++ lib/src/generator/router_generator.dart
@@ -3,7 +3,7 @@ import 'package:build/src/builder/build_step.dart';
 import 'package:code_gen/src/tools/router_collector.dart';
 import 'package:source_gen/source_gen.dart';
 
-import '../annotation/router.dart';
+import '../annotation/router_page.dart';
 
 class RouterGenerator extends GeneratorForAnnotation<RouterPage> {
   static RouterCollector collector = RouterCollector();
